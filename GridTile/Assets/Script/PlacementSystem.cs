f49fb3854b3f5af1302bece82e960f7472735fb6
dani <dani2344@sookmyung.ac.kr>
2024-03-20 23:46:01 +0900

    Update PlacementSystem.cs

--- GridTile/Assets/Script/PlacementSystem.cs
+++ GridTile/Assets/Script/PlacementSystem.cs
@@ -4,94 +4,41 @@ using UnityEngine;
 
 public class PlacementSystem : MonoBehaviour
 {
-    [SerializeField] private GameObject mouseIndicator, cellIndicator;
+    [SerializeField] private GameObject mouseIndicator;
     [SerializeField] private InputManager inputManager;
-    [SerializeField] private Grid grid;
+    [SerializeField] private Grid gridTile;
+    [SerializeField] private Grid gridFurniture;
     [SerializeField] private ObjectsDatabase database;
     private int selectedObjectIndex = -1;
-    [SerializeField] private GameObject gridVisualization;
+    [SerializeField] private GameObject gridTileVisualization;
+    [SerializeField] private GameObject gridFurnVisualization;
     private GridData floorData, furnitureData;
-    private Renderer[] previewRenderer;
+    [SerializeField] private Renderer[] previewRendererTile, previewRendererFurn;
+
     public List<GameObject> placedGameObject;
+    public List<GameObject> placedTile;
 
     [SerializeField] private Transform tileParent;
+
+    [SerializeField] private PreviewSystem previewSystem;
+
+    private Vector3Int lastDetectedPosition = Vector3Int.zero;
+
+    private int rot = 0;
+    private bool isTile =false;
 
     private void Start()
     {
         StopPlacement();
         floorData = new();
         furnitureData = new();
-        previewRenderer = cellIndicator.GetComponentsInChildren<Renderer>();
 
-        GridData startData = floorData;
-        startData.AddObjectAt(new Vector3Int(4, 0, 4), Vector2Int.one, 0, 0, 0);
-        startData.AddObjectAt(new Vector3Int(4, 0, 3), Vector2Int.one, 0, 1, 0);
-        startData.AddObjectAt(new Vector3Int(3, 0, 4), Vector2Int.one, 0, 2, 0);
-        startData.AddObjectAt(new Vector3Int(3, 0, 3), Vector2Int.one, 0, 3, 0);
+        floorData.AddObjectAt(new Vector3Int(4, 0, 4), Vector2Int.one, 0, 0, 0);
+        floorData.AddObjectAt(new Vector3Int(4, 0, 3), Vector2Int.one, 0, 1, 0);
+        floorData.AddObjectAt(new Vector3Int(3, 0, 4), Vector2Int.one, 0, 2, 0);
+        floorData.AddObjectAt(new Vector3Int(3, 0, 3), Vector2Int.one, 0, 3, 0);
+        furnitureData.AddObjectAt(new Vector3Int(6, 0, 6), new Vector2Int(4, 4), 999, placedGameObject.Count-1, 0);
     }
-    public void StartPlacement(int ID)
-    {
-        selectedObjectIndex = database.objectData.FindIndex(data => data.ID == ID);
-        if (selectedObjectIndex < 0)
-        {
-            Debug.Log("읎음");
-            return;
-        }
-        gridVisualization.SetActive(true);
-        cellIndicator.SetActive(true);
-        inputManager.OnClicked += PlaceStructure;
-        inputManager.OnExit += StopPlacement;
-    }
-    private void PlaceStructure()
-    {
-        if (inputManager.IsPointerOverUI())
-        {
-            return;
-        }
-        Vector3 mousePos = inputManager.GetSelectedMapPosition();
-        Vector3Int gridPos = grid.WorldToCell(mousePos);
-
-        bool placementValidity = CheckPlacementValidity(gridPos, selectedObjectIndex);
-        if (placementValidity == false)
-        {
-            return;
-        }
-
-        GameObject newObject = Instantiate(database.objectData[selectedObjectIndex].Prefab);
-        newObject.transform.parent = tileParent;
-        newObject.transform.position = grid.CellToWorld(gridPos) + new Vector3(0, 0.001f, 0);
-        placedGameObject.Add(newObject);
-        GridData selectedData = database.objectData[selectedObjectIndex].ID == 0 ? floorData : furnitureData;
-        selectedData.AddObjectAt(gridPos, database.objectData[selectedObjectIndex].Size, database.objectData[selectedObjectIndex].ID, placedGameObject.Count - 1, 0);
-    }
-    private bool CheckPlacementValidity(Vector3Int gridPos, int selectedObjectIndex)
-    {
-        GridData selectedData = database.objectData[selectedObjectIndex].ID == 0 ? floorData : furnitureData;
-        if (selectedData == floorData) //고른게 tile이면
-        {
-            return selectedData.CanPlaceTileAt(gridPos);
-        }
-        else
-        {
-            if (floorData.placedObjects.ContainsKey(gridPos))
-            {
-                return selectedData.CanPlaceObjectAt(gridPos, database.objectData[selectedObjectIndex].Size);
-            }
-            else
-            {
-                return false;
-            }
-        }
-    }
-    public void StopPlacement()
-    {
-        selectedObjectIndex = -1;
-        gridVisualization.SetActive(false);
-        cellIndicator.SetActive(false);
-        inputManager.OnClicked -= PlaceStructure;
-        inputManager.OnExit -= StopPlacement;
-    }
-
     private void Update()
     {
         if (selectedObjectIndex < 0)
@@ -99,15 +46,161 @@ public class PlacementSystem : MonoBehaviour
             return;
         }
         Vector3 mousePos = inputManager.GetSelectedMapPosition();
-        Vector3Int gridPos = grid.WorldToCell(mousePos);
-
-        bool placementValidity = CheckPlacementValidity(gridPos, selectedObjectIndex);
-        foreach(Renderer render in previewRenderer)
+        Vector3Int gridPos;
+        if (isTile)
         {
-            render.material.color = placementValidity ? Color.white : Color.red;
+            gridPos = gridTile.WorldToCell(mousePos);
+            if (lastDetectedPosition != gridPos)
+            {
+                bool placementValidity = CheckPlacementValidity(gridPos, selectedObjectIndex, rot);
+                foreach (Renderer render in previewRendererTile)
+                {
+                    render.material.color = placementValidity ? Color.white : Color.red;
+                }
+                previewSystem.UpdatePosition(gridTile.CellToWorld(gridPos), placementValidity);
+                lastDetectedPosition = gridPos;
+            }
+        }
+        else
+        {
+            gridPos = gridFurniture.WorldToCell(mousePos);
+            if (lastDetectedPosition != gridPos)
+            {
+                bool placementValidity = CheckPlacementValidity(gridPos, selectedObjectIndex, rot);
+                foreach (Renderer render in previewRendererFurn)
+                {
+                    render.material.color = placementValidity ? Color.white : Color.red;
+                }
+                previewSystem.UpdatePosition(gridFurniture.CellToWorld(gridPos), placementValidity);
+                lastDetectedPosition = gridPos;
+            }
+        }
+        mouseIndicator.transform.position = mousePos;
+    }
+
+    public void StartPlacement(int ID) //놓기 시작
+    {
+        rot = 0;
+        selectedObjectIndex = database.objectData.FindIndex(data => data.ID == ID);
+        if (selectedObjectIndex < 0)
+        {
+            Debug.Log("읎음");
+            return;
+        }
+        if (ID == 0)
+        {
+            gridTileVisualization.SetActive(true);
+            gridFurnVisualization.SetActive(false);
+            isTile = true;
+            previewSystem.StartShowingPlacementPreview(database.objectData[selectedObjectIndex].Prefab, database.objectData[selectedObjectIndex].Size, ID);
+        }
+        else
+        {
+            gridTileVisualization.SetActive(false);
+            gridFurnVisualization.SetActive(true);
+            isTile = false;
+            previewSystem.StartShowingPlacementPreview(database.objectData[selectedObjectIndex].Prefab, database.objectData[selectedObjectIndex].Size, ID);
+        }
+        inputManager.OnClicked += PlaceStructure;
+        inputManager.OnExit += StopPlacement;
+        inputManager.OnRightClicked += RotateStructure;
+    }
+
+    private void PlaceStructure() //클릭으로 놓기
+    {
+        if (inputManager.IsPointerOverUI())
+        {
+            return;
+        }
+        Vector3 mousePos = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPos;
+        if (isTile)
+        {
+            gridPos = gridTile.WorldToCell(mousePos);
+        }
+        else
+        {
+            gridPos = gridFurniture.WorldToCell(mousePos);
         }
 
-        mouseIndicator.transform.position = mousePos;
-        cellIndicator.transform.position = grid.CellToWorld(gridPos);
+        bool placementValidity = CheckPlacementValidity(gridPos, selectedObjectIndex, rot);
+        if (placementValidity == false)
+        {
+            return;
+        }
+
+        GameObject newObject = Instantiate(database.objectData[selectedObjectIndex].Prefab);
+        newObject.transform.parent = tileParent;
+        newObject.transform.rotation = Quaternion.Euler(0, 90 * rot, 0);
+        GridData selectedData = database.objectData[selectedObjectIndex].ID == 0 ? floorData : furnitureData;
+        if (isTile)
+        {
+            newObject.transform.position = gridTile.CellToWorld(gridPos) + new Vector3(0, 0.001f, 0) + previewSystem.offset;
+            placedTile.Add(newObject);
+            selectedData.AddObjectAt(gridPos, database.objectData[selectedObjectIndex].Size, database.objectData[selectedObjectIndex].ID, placedTile.Count - 1, 0);
+            Vector3Int tiles = new Vector3Int(gridPos.x * 2, 0, gridPos.z * 2);
+            furnitureData.AddObjectAt(tiles, new Vector2Int(2, 2), 999, placedGameObject.Count - 1, 0);
+            previewSystem.UpdatePosition(gridTile.CellToWorld(gridPos), false);
+        }
+        else
+        {
+            newObject.transform.position = gridFurniture.CellToWorld(gridPos) + new Vector3(0, 0.001f, 0) + previewSystem.offset;
+            if (rot == 1)
+            {
+                newObject.transform.position += new Vector3(0, 0, 1);
+            }
+            else if (rot == 2)
+            {
+                newObject.transform.position += new Vector3(0, 0, 0);
+            }
+            else if (rot == 3)
+            {
+                newObject.transform.position += new Vector3(0, 0, -1);
+            }
+            placedGameObject.Add(newObject);
+            selectedData.AddObjectAt(gridPos, database.objectData[selectedObjectIndex].Size, database.objectData[selectedObjectIndex].ID, placedGameObject.Count - 1, rot);
+            previewSystem.UpdatePosition(gridFurniture.CellToWorld(gridPos), false);
+        }
+    }
+
+    private bool CheckPlacementValidity(Vector3Int gridPos, int selectedObjectIndex, int rot)
+    {
+        GridData selectedData = database.objectData[selectedObjectIndex].ID == 0 ? floorData : furnitureData;
+        if (selectedData == floorData) //고른게 tile이면
+        {
+            return selectedData.CanPlaceTileAt(gridPos);
+        }
+        else //고른게 가구라면
+        {
+            if (furnitureData.placedTiles.ContainsKey(gridPos))
+            {
+                return selectedData.CanPlaceObjectAt(gridPos, database.objectData[selectedObjectIndex].Size, rot);
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public void StopPlacement() //설치 멈추기
+    {
+        previewSystem.StopShowingPreview();
+        lastDetectedPosition = Vector3Int.zero;
+        selectedObjectIndex = -1;
+        gridTileVisualization.SetActive(false);
+        gridFurnVisualization.SetActive(false);
+        inputManager.OnClicked -= PlaceStructure;
+        inputManager.OnExit -= StopPlacement;
+        inputManager.OnRightClicked -= RotateStructure;
+        rot = 0;
+    }
+
+    public void RotateStructure()
+    {
+        rot++;
+        rot = rot % 4; //0~3까지만
+        Debug.Log(rot);
+        previewSystem.RotatePreview(rot);
     }
 }
